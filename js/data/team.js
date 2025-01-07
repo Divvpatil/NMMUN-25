738beeb9434fddcb472d13fad8293bf3a3349e00
Divyanshu Patil <patildivyanshu3@gmail.com>
2025-01-07 20:08:14 +0530

    updated team js

--- js/data/team.js
+++ js/data/team.js
@@ -7,32 +7,32 @@ export const secretariat = [
   {
     name: "Aaditya Patil | Vrudhi Varma",
     position: "Deputy Secretary General",
-    image: "Images/DSG.jpg"
+    image: "/Images/DSG.jpg"
   },
   {
     name: "Owais Bubere",
     position: "Director General",
-    image: "Images/DG.jpg"
+    image: "/Images/DG.jpg"
   },
   {
     name: "Aastha Tiwari | Nitya Chourasia",
     position: "Deputy Director General",
-    image: "Images/DDG.jpg"
+    image: "/Images/DDG.jpg"
   },
   {
     name: "Nishtha Desai",
     position: "Chief Relations Officer",
-    image: "Images/CRO.jpg"
+    image: "/Images/CRO.jpg"
   },
   {
     name: "Meet Maheshwari",
     position: "Charge D' Affaires",
-    image: "Images/CDA.jpg"
+    image: "/Images/CDA.jpg"
   },
   {
     name: "Rasanath Kulkarni | Avneesh Taneja | Rushikesh Patil",
     position: "Planning & Strategy",
-    image: "Images/PS.jpg"
+    image: "/Images/PS.jpg"
   }
 ];
 
@@ -40,41 +40,41 @@ export const directorate = [
   {
     name: "Divyanshu Patil",
     position: "Technical",
-    image: "Images/Tech.jpg"
+    image: "/Images/Tech.jpg"
   },
   {
     name: "Dhananjay Khairnar",
     position: "SMM",
-    image: "Images/SMM.jpg"
+    image: "/Images/SMM.jpg"
   },
   {
     name: "Malay Padshah",
     position: "Media",
-    image: "Images/Media.jpg"
+    image: "/Images/Media.jpg"
   },
   {
     name: "Navya Singh | Sanskriti Kuwar",
     position: "Documentation",
-    image: "Images/Docu.jpg"
+    image: "/Images/Docu.jpg"
   },
   {
     name: "Arshad Khursheed | Allan Jose",
     position: "Logistics",
-    image: "Images/Logi.jpg"
+    image: "/Images/Logi.jpg"
   },
   {
     name: "Dhruv Singh | Khushi Rathi | Niti Sharma",
     position: "DSNH",
-    image: "Images/DSNH.jpg"
+    image: "/Images/DSNH.jpg"
   },
   {
     name: "Durwa upadhyay | Ronak Vishanwani | Purvesh Patil",
     position: "Marketing",
-    image: "Images/Marketing.jpg"
+    image: "/Images/Marketing.jpg"
   },
   {
     name: "Rushabh Shah",
     position: "Corporate Outreach",
-    image: "Images/CO.jpg"
+    image: "/Images/CO.jpg"
   }
 ];
