0121b06bec20980eef2181357a652c7dd74e5145
Divyanshu Patil <patildivyanshu3@gmail.com>
2025-01-15 17:30:38 +0530

    home page image updates

--- js/data/about.js
+++ js/data/about.js
@@ -2,16 +2,16 @@ export const aboutData = {
   history: {
     title: "Our History",
     content: "In the vibrant year of 2008, Narsee Monjee Model United Nations emerged from the fiery crucible of shared dreams and ambitions. A small band of visionaries joined forces to breathe life into an official Model United Nations. In mere moments of inspiration, we embarked on a journey that would ignite a remarkable transformation. Over the years, our evolution has been nothing short of extraordinary. What began as a modest endeavor has now blossomed into a dynamic and mature organization. We've harnessed our passions to transcend boundaries and create an empire of opportunities. When you choose to walk this path with us, you'll not only gain access to a multitude of riveting events but also unlock the door to a vibrant community pulsating with the same fervor as yours. It's not just an organization; it's a force of nature. Join us, and let's write history together.",
-    image: "images/history.jpg"
+    image: "/Images/logo.png"
   },
   campus: {
     title: "About NMIMS Shirpur",
     content: "SVKM's NMIMS, Mukesh Patel Technology Park, Shirpur campus is an integrated campus approved by All India Council for Technical Education (AICTE), New Delhi. SVKM'S NMIMS Shirpur Campus is an affiliate of NMIMS (Deemed-to-be-University), which has been accredited with A+ Grade by National Assessment and Accreditation Council (NAAC) and has been recognized as a Tier 1 University by University Grants Commission (UGC).",
-    image: "images/campus.jpg"
+    image: "/Images/clg.png"
   },
   theme: {
     title: "Diplomacy without Borders",
     content: "The tagline \"Diplomacy Without Borders\" encapsulates the spirit of NM Model United Nations (NMMUN). It signifies the breaking down of geographical, cultural, and ideological barriers to foster meaningful dialogue, collaboration, and understanding. This phrase highlights the essence of MUN, where delegates come together from diverse backgrounds to discuss global issues, propose solutions, and practice diplomacy that transcends boundaries. It represents a vision of a unified world driven by cooperation, critical thinking, and respect for diverse perspectives, reflecting the core values of NMMUN.",
-    image: "images/peace.jpg"
+    image: "/Images/tagline.png"
   }
 };
